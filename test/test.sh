712098bf0689c172fad3ee4d90373acb968598d4
hyi <hongyi@renci.org>
2020-09-09 17:59:18 -0400

    fixing tests

--- test/test.sh
+++ test/test.sh
@@ -4,4 +4,4 @@ set -o allexport
 source env.docker
 set +o allexport
 
-docker-compose -f docker-compose.yml -f test/docker-compose.yml up --build -V --exit-code-from tx-autht
+docker-compose -f docker-compose.yml -f test/docker-compose.yml up --build -V --exit-code-from txautht
